84ff3de0118768391ac64ff7e1f6c6e42928c5e7
Burak Ince <burak.ince@linux.org.tr>
2020-10-06 18:44:05 +0200

    Remove provider details

--- stage/main.tf
+++ stage/main.tf
@@ -1,17 +1,10 @@
 terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = ">= 2.26"
-    }
-  }
   backend "azurerm" {
     resource_group_name  = "burak-tfstate-rg"
     storage_account_name = "buraktfstate2369"
     container_name       = "tfstate"
     key                  = "stage.terraform.tfstate"
   }
-
 }
 
 provider "azurerm" {
