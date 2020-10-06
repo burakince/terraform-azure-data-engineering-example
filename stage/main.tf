6fdbab7a509ea812e3a2f2ff65ee44aef7aedb85
Burak Ince <burak.ince@linux.org.tr>
2020-10-06 18:29:37 +0200

    Add variable definition again

--- stage/main.tf
+++ stage/main.tf
@@ -15,6 +15,11 @@ terraform {
 }
 
 provider "azurerm" {
+  tenant_id       = "${var.tenant_id}"
+  subscription_id = "${var.subscription_id}"
+  client_id       = "${var.agent_client_id}"
+  client_secret   = "${var.agent_client_secret}"
+
   environment     = "public"
   features {}
 }
